5308902c8bd0d41d47b72fe168b19eedf799920e
oliwoodsuk <55204545+oliwoodsuk@users.noreply.github.com>
2025-06-01 11:34:35 +0200

    initial commit

---
+++ woo-preproduct.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       PreProduct
+ * Plugin URI:        https://preproduct.io/woocommerce-pre-orders/
+ * Description:       Smarter WooCommerce pre-orders. Charge upfront, later or both (via deposits), add a pre-order area to your cart page, automations available as well as fine-grained control.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            PreProduct
+ * Author URI:        https://preproduct.io/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ <!-- * Update URI:        -->
+ <!-- * Text Domain:        -->
+ <!-- * Domain Path:       /languages -->
+ */
